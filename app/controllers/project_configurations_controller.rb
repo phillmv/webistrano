3784348e219488477dfa6483dfe20a52a792a839
Phill <phill.mv@gmail.com>
2008-10-01 15:11:30 -0400

    Now that there's a typo.

--- app/controllers/project_configurations_controller.rb
+++ app/controllers/project_configurations_controller.rb
@@ -1,6 +1,6 @@
 class ProjectConfigurationsController < ApplicationController
   
-  before_filter :load_project, :except => [:new, :dashboard, :index, :create]t
+  before_filter :load_project, :except => [:new, :dashboard, :index, :create]
   
   # GET /projects/1/project_configurations/1
   # GET /projects/1/project_configurations/1.xml
